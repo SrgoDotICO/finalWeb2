fc46ac9d6e8864988ff0f91ef3b69d364b2d9269
SrgoDotICO <anubarack95@gmail.com>
2023-05-24 20:02:05 -0600

    Creacion de header y footer para agregar estilos al en cabezado y footer tambien se agregan carpetas de materialize y recursos media

---
+++ footer.php
@@ -0,0 +1,27 @@
+<footer class="page-footer orange">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">Company Bio</h5>
+          <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+      Made by <a class="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+      </div>
+    </div>
+</footer>
+
+
+ <!--JavaScript at end of body for optimized loading-->
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+ <script type="text/javascript" src="js/materialize.min.js"></script>
+ <script>
+  document.addEventListener('DOMContentLoaded', function() {
+     M.AutoInit();
+  });
+ </script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="./Recursos/Media/favicon.jpg" type="image/x-icon">
+    <!--Materialize-->
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>ICO FES ARAGON</title>
+</head>
+
+<body>
+
+   <div class="navbar-fixed">
+   <nav class="nav-wrapper light-blue darken-2">
+     <div class="container">
+     <a href="#" class="brand-logo">FES ARAGON</a>
